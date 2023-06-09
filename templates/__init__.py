0588b042892d838d0134e9945e12a49342aa7773
Khiem Doan <doankhiem.crazy@gmail.com>
2023-06-09 10:57:24 +0700

    Add functions to render templates

---
+++ templates/__init__.py
@@ -0,0 +1,22 @@
+
+__author__ = 'Khiem Doan'
+__github__ = 'https://github.com/khiemdoan'
+__email__ = 'doankhiem.crazy@gmail.com'
+
+import asyncio
+from pathlib import Path
+from typing import Any
+
+from jinja2 import Environment, FileSystemLoader
+
+this_dir = Path(__file__).parent
+env = Environment(loader=FileSystemLoader(this_dir))
+
+
+def render(file: str, context: dict[str, Any] = {}, **kwargs) -> str:
+    template = env.get_template(file)
+    return template.render(context, **kwargs)
+
+
+async def arender(file: str, context: dict[str, Any] = {}, **kwargs) -> str:
+    return await asyncio.to_thread(render, file, context, **kwargs)
