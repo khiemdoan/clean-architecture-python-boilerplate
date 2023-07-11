260839bf20e73f2f697a6ccf445786593c7d2954
Khiem Doan <doankhiem.crazy@gmail.com>
2023-07-11 11:11:02 +0700

    Add timeit decorator

--- decorators/__init__.py
+++ decorators/__init__.py
@@ -3,7 +3,11 @@ __author__ = 'Khiem Doan'
 __github__ = 'https://github.com/khiemdoan'
 __email__ = 'doankhiem.crazy@gmail.com'
 
-__all__ = ['ignore_exception', 'ignore_exception_with_logger']
+__all__ = [
+    'ignore_exception',
+    'timeit',
+]
 
 
-from .exception import ignore_exception, ignore_exception_with_logger
+from .exception import ignore_exception
+from .timeit import timeit
