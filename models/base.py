da622e4c7fd80cc68d1fd72c749196409a44357d
Khiem Doan <doankhiem.crazy@gmail.com>
2023-09-25 15:44:51 +0700

    Update base model

--- models/base.py
+++ models/base.py
@@ -3,26 +3,51 @@ __author__ = 'Khiem Doan'
 __github__ = 'https://github.com/khiemdoan'
 __email__ = 'doankhiem.crazy@gmail.com'
 
-# Original source from `litestar` project
-# https://github.com/litestar-org/litestar/blob/main/litestar/contrib/sqlalchemy/base.py
+# Original source from `advanced-alchemy` project
+# https://github.com/jolt-org/advanced-alchemy/blob/main/advanced_alchemy/base.py
 # Modified by: Khiem Doan
+# URL: https://github.com/khiemdoan/clean-architecture-python-boilerplate/blob/main/models/base.py
 
 """Application ORM configuration."""
 
+import contextlib
 import re
 from datetime import date, datetime, timezone
-from typing import Any, ClassVar, Protocol, runtime_checkable
+from typing import Any, ClassVar, Protocol, TypeVar, runtime_checkable
 from uuid import UUID, uuid4
 
-from pydantic import AnyHttpUrl, AnyUrl, EmailStr
-from sqlalchemy import BigInteger, Date, DateTime, MetaData, String, func
-from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column, registry
+from advanced_alchemy.types import GUID, BigIntIdentity, DateTimeUTC, JsonB
+from sqlalchemy import Date, MetaData, Sequence, String, func
+from sqlalchemy.event import listens_for
+from sqlalchemy.orm import DeclarativeBase, Mapped, Session, declared_attr, mapped_column, orm_insert_sentinel, registry
 from sqlalchemy.sql import FromClause
+from sqlalchemy.sql.schema import _NamingSchemaParameter as NamingSchemaParameter
+from sqlalchemy.types import TypeEngine
+
+__all__ = (
+    "AssociationAuditBase",
+    "AssociationBase",
+    "AuditColumns",
+    "BigIntAuditBase",
+    "BigIntBase",
+    "BigIntPrimaryKey",
+    "CommonTableAttributes",
+    "create_registry",
+    "ModelProtocol",
+    "touch_updated_timestamp",
+    "UUIDAuditBase",
+    "UUIDBase",
+    "UUIDPrimaryKey",
+)
+
+
+UUIDBaseT = TypeVar("UUIDBaseT", bound="UUIDBase")
+BigIntBaseT = TypeVar("BigIntBaseT", bound="BigIntBase")
 
 same_as = lambda col: lambda ctx: ctx.current_parameters.get(col)
 
 
-convention = {
+convention: NamingSchemaParameter = {
     "ix": "ix_%(column_0_label)s",
     "uq": "uq_%(table_name)s_%(column_0_name)s",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
@@ -30,6 +55,23 @@ convention = {
     "pk": "pk_%(table_name)s",
 }
 """Templates for automated constraint name generation."""
+
+
+@listens_for(Session, "before_flush")
+def touch_updated_timestamp(session: Session, *_: Any) -> None:
+    """Set timestamp on update.
+
+    Called from SQLAlchemy's
+    :meth:`before_flush <sqlalchemy.orm.SessionEvents.before_flush>` event to bump the ``updated``
+    timestamp on modified instances.
+
+    Args:
+        session: The sync :class:`Session <sqlalchemy.orm.Session>` instance that underlies the async
+            session.
+    """
+    for instance in session.dirty:
+        if hasattr(instance, "updated_at"):
+            instance.updated_at = datetime.now(timezone.utc)
 
 
 @runtime_checkable
@@ -51,29 +93,42 @@ class ModelProtocol(Protocol):
 class UUIDPrimaryKey:
     """UUID Primary Key Field Mixin."""
 
-    id: Mapped[UUID] = mapped_column(server_default=func.gen_random_uuid(), default=uuid4, primary_key=True)  # pyright: ignore
+    id: Mapped[UUID] = mapped_column(server_default=func.gen_random_uuid(), default=uuid4, primary_key=True)
     """UUID Primary key column."""
+
+    # noinspection PyMethodParameters
+    @declared_attr
+    def _sentinel(cls) -> Mapped[int]:
+        return orm_insert_sentinel(name="sa_orm_sentinel")
 
 
 class BigIntPrimaryKey:
     """BigInt Primary Key Field Mixin."""
 
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    # noinspection PyMethodParameters
+    @declared_attr
+    def id(cls) -> Mapped[int]:
+        """BigInt Primary key column."""
+        return mapped_column(
+            BigIntIdentity,
+            Sequence(f"{cls.__tablename__}_id_seq", optional=False),  # type: ignore[attr-defined]
+            primary_key=True,
+        )
+
 
 class AuditColumns:
     """Created/Updated At Fields Mixin."""
 
-    created_at: Mapped[datetime] = mapped_column(  # pyright: ignore
-        DateTime(timezone=True),
+    created_at: Mapped[datetime] = mapped_column(
+        DateTimeUTC(timezone=True),
         server_default=func.now(),
         default=lambda: datetime.now(timezone.utc),
     )
     """Date/time of instance creation."""
-    updated_at: Mapped[datetime] = mapped_column(  # pyright: ignore
-        DateTime(timezone=True),
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTimeUTC(timezone=True),
         server_default=func.now(),
         default=lambda: datetime.now(timezone.utc),
-        onupdate=lambda: datetime.now(timezone.utc),
     )
     """Date/time of instance last update."""
 
@@ -81,15 +136,17 @@ class AuditColumns:
 def create_registry() -> registry:
     """Create a new SQLAlchemy registry."""
     meta = MetaData(naming_convention=convention)
-    return registry(
-        metadata=meta,
-        type_annotation_map={
-            EmailStr: String,
-            AnyUrl: String,
-            AnyHttpUrl: String,
-            date: Date,
-        },
-    )
+    type_annotation_map: dict[type, type[TypeEngine[Any]] | TypeEngine[Any]] = {
+        UUID: GUID,
+        datetime: DateTimeUTC,
+        date: Date,
+        dict: JsonB,
+    }
+    with contextlib.suppress(ImportError):
+        from pydantic import AnyHttpUrl, AnyUrl, EmailStr
+
+        type_annotation_map.update({EmailStr: String, AnyUrl: String, AnyHttpUrl: String})
+    return registry(metadata=meta, type_annotation_map=type_annotation_map)
 
 
 orm_registry = create_registry()
