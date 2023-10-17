941fc9a561c07adf95317dbfc4ed82aa8047ca38
Khiem Doan <doankhiem.crazy@gmail.com>
2023-10-17 10:25:13 +0700

    Restruct project

--- src/models/base.py
+++ src/models/base.py
@@ -2,7 +2,7 @@
 __author__ = 'Khiem Doan'
 __github__ = 'https://github.com/khiemdoan'
 __email__ = 'doankhiem.crazy@gmail.com'
-__url__ = 'https://github.com/khiemdoan/clean-architecture-python-boilerplate/blob/main/models/base.py'
+__url__ = 'https://github.com/khiemdoan/clean-architecture-python-boilerplate/blob/main/src/models/base.py'
 
 # Original source from `advanced-alchemy` project
 # https://github.com/jolt-org/advanced-alchemy/blob/main/advanced_alchemy/base.py
--- src/services/__init__.py
+++ src/services/__init__.py
@@ -2,8 +2,11 @@
 __author__ = 'Khiem Doan'
 __github__ = 'https://github.com/khiemdoan'
 __email__ = 'doankhiem.crazy@gmail.com'
+__url__ = 'https://github.com/khiemdoan/clean-architecture-python-boilerplate/blob/main/src/services/__init__.py'
 
-__all__ = ['Database']
+__all__ = [
+    'Database',
+]
 
 
 from .database import Database
