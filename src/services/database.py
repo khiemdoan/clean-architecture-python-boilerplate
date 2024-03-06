8cfb93dff73c19ab0e190a84a86e6dee4c08d37b
Khiem Doan <doankhiem.crazy@gmail.com>
2024-03-06 09:14:10 +0700

    Now database can be used in both sync and async

--- src/services/database.py
+++ src/services/database.py
@@ -1,20 +1,20 @@
-
 __author__ = 'Khiem Doan'
 __github__ = 'https://github.com/khiemdoan'
 __email__ = 'doankhiem.crazy@gmail.com'
 __url__ = 'https://github.com/khiemdoan/clean-architecture-python-boilerplate/blob/main/src/services/database.py'
 
 import inspect
-import logging
-from contextlib import AbstractAsyncContextManager
+from asyncio import current_task
+from contextlib import AbstractAsyncContextManager, AbstractContextManager
 from functools import wraps
+from inspect import iscoroutinefunction
 from typing import Callable, ParamSpec, TypeVar
 
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_scoped_session, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import Session, scoped_session, sessionmaker
 
 from settings import get_postgres_settings
-
-logger = logging.getLogger(__name__)
 
 Param = ParamSpec('Param')
 RetType = TypeVar('RetType')
@@ -22,50 +22,82 @@ OriginalFunc = Callable[Param, RetType]
 DecoratedFunc = Callable[Param, RetType]
 
 
-class Database(AbstractAsyncContextManager):
-
-    _factory: async_sessionmaker|None = None
+class Database(AbstractContextManager, AbstractAsyncContextManager):
+    _SyncScopedSession: scoped_session[Session] | None = None
+    _AsyncScopedSession: async_scoped_session[AsyncSession] | None = None
 
     @classmethod
     def connect(cls) -> None:
-        if cls._factory:
+        if cls._SyncScopedSession and cls._AsyncScopedSession:
             return
+
         settings = get_postgres_settings()
-        engine = create_async_engine(settings.url, echo=settings.debug)
-        cls._factory = async_sessionmaker(bind=engine)
+        options = {
+            'pool_size': 10,
+            'max_overflow': 10,
+        }
+
+        sync_engine = create_engine(settings.url, echo=settings.debug, **options)
+        sync_factory = sessionmaker(bind=sync_engine)
+        cls._SyncScopedSession = scoped_session(sync_factory)
+
+        async_engine = create_async_engine(settings.url, echo=settings.debug, **options)
+        async_factory = async_sessionmaker(bind=async_engine)
+        cls._AsyncScopedSession = async_scoped_session(async_factory, current_task)
 
     @classmethod
     def decorator(cls, func) -> Callable[[OriginalFunc], DecoratedFunc]:
-        @wraps(func)
-        async def wrapper(*args, **kwargs):
-            cls.connect()
-            async with cls._factory() as session:
-                fsig = inspect.signature(func)
-                for _, param in fsig.parameters.items():
-                    if param.annotation == AsyncSession:
-                        kwargs[param.name] = session
-                result = await func(*args, **kwargs)
-                try:
-                    await session.commit()
-                except Exception as ex:
-                    logger.error(ex)
-                    await session.rollback()
-                finally:
-                    await session.close()
-            return result
+        fsig = inspect.signature(func)
 
-        return wrapper
+        if iscoroutinefunction(func):
+
+            @wraps(func)
+            async def wrapper(*args, **kwargs):
+                cls.connect()
+                session = cls._AsyncScopedSession()
+
+                # Prepare params
+                for _, param in fsig.parameters.items():
+                    if param.annotation == type(session):
+                        kwargs[param.name] = session
+
+                # Call function
+                try:
+                    return await func(*args, **kwargs)
+                finally:
+                    await cls._AsyncScopedSession.remove()
+
+            return wrapper
+        else:
+
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                cls.connect()
+                session = cls._SyncScopedSession()
+
+                # Prepare params
+                for _, param in fsig.parameters.items():
+                    if param.annotation == type(session):
+                        kwargs[param.name] = session
+
+                # Call function
+                try:
+                    return func(*args, **kwargs)
+                finally:
+                    cls._SyncScopedSession.remove()
+
+            return wrapper
+
+    def __enter__(self) -> Session:
+        self.connect()
+        return self._SyncScopedSession()
+
+    def __exit__(self, *exc) -> None:
+        self._SyncScopedSession.remove()
 
     async def __aenter__(self) -> AsyncSession:
         self.connect()
-        self._session = self._factory()
-        return self._session
+        return self._AsyncScopedSession()
 
-    async def __aexit__(self, *exc):
-        try:
-            await self._session.commit()
-        except Exception as ex:
-            logger.error(ex)
-            await self._session.rollback()
-        finally:
-            await self._session.close()
+    async def __aexit__(self, *exc) -> None:
+        await self._AsyncScopedSession.remove()
